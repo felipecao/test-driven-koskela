655c7ceb6ec7d5af7420ada83effec9c513ea1e6
Felipe Carvalho <felipe.carvalho@gmail.com>
2016-10-16 19:20:25 +0200

    add TestTemplate in a red state

---
+++ src/main/java/Template.java
@@ -0,0 +1,13 @@
+public class Template {
+
+    public Template(String templateText) {
+    }
+
+    public void set(String variable, String value) {
+
+    }
+
+    public String evaluate() {
+        return null;
+    }
+}
---
+++ src/test/java/TestTemplate.java
@@ -0,0 +1,16 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestTemplate {
+
+    @Test
+    public void oneVariable() {
+        Template template = new Template("Hello, ${name}");
+
+        template.set("name", "Reader");
+
+        assertEquals("Hello, Reader", template.evaluate());
+    }
+
+}
